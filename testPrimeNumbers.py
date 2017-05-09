2917e8970435c43b2c5fb6cf4a3dfe602c7dd3f6
charlieoduk <odukjr@gmail.com>
2017-05-09 04:30:00 +0300

    updated test cases

--- testPrimeNumbers.py
+++ testPrimeNumbers.py
@@ -1,17 +1,11 @@
 import unittest
 import PrimeNumbers
 
-# testcases
 
-# 1. When someone enters 1
-# 2. When someone enters a -ve number
-# 3. Enter a range and test the expected outputs
-# 4. When a string is entered instead of an int
-# 5. when number is a decimal or fraction
 
 class primeTestCases(object):
     def setUp(self):
-        self.prime = PrimeNumbers.list_prime()
+        self.prime = PrimeNumbers.num_types()
 
     
     def test_list_prime_check_expected_output(self):
